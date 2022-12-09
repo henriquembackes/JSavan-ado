15e4ba113a3dd2e51d3743697eae0b5e453bae98
henriquembackes <henriquebakizao@gmail.com>
2022-12-09 19:07:40 -0300

    att

---
+++ aula_02/cervejas.js
@@ -0,0 +1,26 @@
+const cervejas = ['skol', 'spaten', 'bud', 'kaiser', 'becks', 'corona', 'polar'];
+
+// CRUD
+function create(){
+
+};
+
+function read(){
+    return cervejas;
+};
+
+function uptade(){
+
+};
+
+//delete palavra reservada - por isso remove
+function remove(){
+
+};
+
+module.exports = {
+    create,
+    read,
+    uptade,
+    remove
+};
